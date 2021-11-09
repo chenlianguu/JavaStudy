749032da861878947c85256bd9e4bbe2b3145495
jacky <chenlianguu@gmail.com>
2021-11-09 23:00:13 +0800

    创建线程5种方式

--- src/com/alevelhome/juc/c001/T02_HowToCreateThread.java
+++ src/com/alevelhome/juc/c001/T02_HowToCreateThread.java
@@ -1,6 +1,10 @@
 package com.alevelhome.juc.c001;
 
 
+import sun.nio.ch.ThreadPool;
+
+import java.util.concurrent.*;
+
 /**
  * 创建线程的5类方式：
  * 1 继承thread类
@@ -20,12 +24,56 @@ public class T02_HowToCreateThread {
     private static class Thread02 implements Runnable {
         @Override
         public void run() {
-            System.out.println("Thead02");
+            System.out.println("Thread02");
+        }
+    }
+
+    private static class Thread04 implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Thread04");
+        }
+    }
+
+    private static class Thread05 implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            System.out.println("Thread05");
+            return "return Thread05";
         }
     }
 
     public static void main(String[] args) {
         // 方式一
+        new Thread01().start();
+
+        // 方式二
+        new Thread(new Thread02()).start();
+
+        // 方式三
+        new Thread(() -> {
+            System.out.println("Thread03");
+        } ).start();
+
+        // 方式四
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.execute(new Thread04());
+
+        // 方式五
+        FutureTask task = new FutureTask<String>(new Thread05());
+        Thread thread04 = new Thread(task);
+        thread04.start();
+
+        try {
+            String result = (String) task.get();
+            System.out.println(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
